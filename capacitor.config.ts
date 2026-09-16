575167b7d838797fa3acd6a18e98bf2feffe1083
Nino Herenda <ninogor@gmail.com>
2026-09-16 10:51:19 +0200

    android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'fit.elitebounce',
   appName: 'Elite Bounce',
-  webDir: 'public'
+  webDir: 'public',
+  server: {
+    url: 'https://elitebounce.fit/login', // <--- Cilja tvoj live sajt
+    cleartext: true
+  }
 };
 
 export default config;
